b014a642f24c29619e42ff88c3ef608f6470ce78
VatsalChamaria <VATSALCHAMARIA28@GMAIL.COM>
2025-06-11 16:54:13 +0530

    Create Middleware.cs

---
+++ Middleware.cs
@@ -0,0 +1,8 @@
+public class LoggingMiddleware
+{
+    private readonly RequestDelegate _next;
+    public LoggingMiddleware(RequestDelegate next) => _next = next;
+
+    public async Task Invoke(HttpContext context)
+    {
+        Console.WriteLine($"[Request
